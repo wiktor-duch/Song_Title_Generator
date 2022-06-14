f7a446068e5648fd59a81d6fb6c52af62d30fbb0
wiktor-duch <wiktorduch22@gmail.com>
2022-06-14 16:21:25 +0100

    Add user input and vizualization module
    The user can know enter how many words should be pulled from Random
    Word API. The input is validated if it is a number between 5 and 20 and
    is an integer.
    
    Vizualization class for now has only one function that displays the
    pulled words to user in a more readable way.

--- main.py
+++ main.py
@@ -3,13 +3,26 @@
 from exceptions import RandomWordAPIException
 from random_word_api_handler import RandomWordAPIHandler
 import sys
+from visualization import display_words
 
 if __name__ == "__main__":
+    print("Song Title Generator\n")
 
+    # Ask the user for number of random words
     while True:
         try:
-            random_words = RandomWordAPIHandler().get_random_words(10)
-            print(random_words)
+            num_words = int(input("How many different words would you like to generate [5-20]? ").strip())
+            if num_words >= 5 and num_words <= 20:
+                break
+            else:
+                print("Entered number must be between 5 and 20!")
+        except ValueError:
+            print('You must enter a number between 5 and 20!')
+
+    # Get the required number of random words from the Random Word API
+    while True:
+        try:
+            random_words = RandomWordAPIHandler().get_random_words(num_words)
             break
         except RandomWordAPIException:
             # Ask the user to try again
@@ -20,4 +33,5 @@ if __name__ == "__main__":
             if answer not in ["Yes", "yes", "Y", "y"]:
                 sys.exit()
 
-    print('the rest of the code')
+    # Display the words to the user
+    display_words(random_words)
