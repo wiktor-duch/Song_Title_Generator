b9b91e6a26fda87edbce49b997c47cf200968924
wiktor-duch <wiktorduch22@gmail.com>
2022-06-14 11:39:25 +0100

    Update Random Word API handler
    get_random_words function now returns a sorted list of random words and
    ensures that the number of unique words is as specified.

--- random_word_api_handler.py
+++ random_word_api_handler.py
@@ -10,11 +10,33 @@ class RandomWordAPIHandler:
         pass
 
     def get_random_words(self, num:int) -> List[str]:
+        random_words = set()
+
         # Query the Random Word API
         response = requests.get("https://random-word-api.herokuapp.com/word?number=" + str(num))
         
         # Check if the query was successful
-        if response.status_code != 200:
-            raise RandomWordAPIException(f"Error: Random Words API query returned {response.status_code}")
+        if response.status_code == 200:
+            random_words = set(response.json())
+        else:
+            raise RandomWordAPIException(f"Error: Random Words API query returned {response.status_code}.")
 
-        return response.json()
+        if len(random_words) >= num:
+            return sorted(random_words)
+        else:
+            # For loop ensures that it does not query API infinitely
+            for _ in range(100):
+                # Query the Random Word API
+                response = requests.get("https://random-word-api.herokuapp.com/word")
+                
+                # Check if the query was successful
+                if response.status_code != 200:
+                    raise RandomWordAPIException(f"Error: Random Words API query returned {response.status_code}.")
+
+                random_words.add(response.json()[0])
+                
+                if len(random_words) >= num:
+                    return self.sorted(random_words)
+        
+        # Raise exception if the for loop has terminated
+        raise RandomWordAPIException(f"Error: Could not find the required number of unique words ({num}).")
